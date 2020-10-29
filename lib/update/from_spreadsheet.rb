bd750cf609e282d550ce201730b91a22e94bb2cd
Jacky Zhu <jackyzhu@berkeley.edu>
2020-10-28 23:01:35 -0700

    Rake task that updates db based on spreadsheet

--- lib/update/from_spreadsheet.rb
+++ lib/update/from_spreadsheet.rb
@@ -5,13 +5,20 @@ module Update
         
         def FromSpreadsheet.load
             session = GoogleDrive::Session.from_service_account_key("election-tracker-19a00db0d21f.json")
-
-            # Get the spreadsheet by its title
             spreadsheet = session.spreadsheet_by_title("Live Results")
-            # Get the first worksheet
-            worksheet = spreadsheet.worksheets.first
-            # Print out the first 6 columns of each row
-            worksheet.rows.each { |row| puts row.first(6).join(" | ") }
+
+            spreadsheet.worksheets.each do |ws|
+                state_name = ws.title
+                state = State.find_by(slug: state_name.parameterize)
+
+                ws.list.each do |row|
+                    county_name = row["County"]
+                    county = state.counties.find_by(slug: county_name.parameterize)
+
+                    county.update({:dem => row["Biden"], :gop => row["Trump"], :percentage_precincts_reporting => row["Percentage Reporting"]})
+                end
+
+            end
         end
     end
 end

5659835c4dad8afb6b517127f1ac549dc9fc2422
Jacky Zhu <jackyzhu@berkeley.edu>
2020-10-28 21:38:45 -0700

    seed spawns 0 data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,26 +13,46 @@ Madison.states.map do |state|
 end
 
 curDir = File.dirname(__FILE__)
-path_to_seed_data = File.join(curDir, "2016_US_County_Level_Presidential_Results.csv")
-results = CSV.parse(File.read(path_to_seed_data), headers: true)
-puts results.length
-results.map do |entry|
-    full_state_name = Madison.get_name(entry['state_abbr'])
+county_names = File.join(curDir, "counties.csv")
+counties = CSV.parse(File.read(county_names), headers: true)
+puts counties.length
+counties.map do |entry|
+    full_state_name = Madison.get_name(entry['state'])
     state = State.find_by(slug: full_state_name.parameterize)
-    percentage_precincts_reporting = rand(100)
-    # total_precincts = rand(100)
-    # precincts_reporting = rand(total_precincts)
-    county_name = entry['county_name']
+    county_name = entry['name']
     if county_name.include?(" County")
         county_name = county_name.split(" County").first
     end
 
     County.create(name: county_name, 
-        dem: entry['votes_dem'], 
-        gop: entry['votes_gop'], 
+        dem: 0,
+        gop: 0,
         state_id: state.id, 
-        percentage_precincts_reporting: percentage_precincts_reporting
-        # precincts_reporting: precincts_reporting,
-        # total_precincts: total_precincts
+        percentage_precincts_reporting: 0,
     )
+
 end
+# curDir = File.dirname(__FILE__)
+# path_to_seed_data = File.join(curDir, "2016_US_County_Level_Presidential_Results.csv")
+# results = CSV.parse(File.read(path_to_seed_data), headers: true)
+# puts results.length
+# results.map do |entry|
+#     full_state_name = Madison.get_name(entry['state_abbr'])
+#     state = State.find_by(slug: full_state_name.parameterize)
+#     percentage_precincts_reporting = rand(100)
+#     # total_precincts = rand(100)
+#     # precincts_reporting = rand(total_precincts)
+#     county_name = entry['county_name']
+#     if county_name.include?(" County")
+#         county_name = county_name.split(" County").first
+#     end
+
+    # County.create(name: county_name, 
+    #     dem: entry['votes_dem'], 
+    #     gop: entry['votes_gop'], 
+    #     state_id: state.id, 
+    #     percentage_precincts_reporting: percentage_precincts_reporting
+    #     # precincts_reporting: precincts_reporting,
+    #     # total_precincts: total_precincts
+    # )
+# end
